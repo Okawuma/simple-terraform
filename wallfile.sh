a15c8fb866c7d820d64f0537353b906925324ea0
Otillia <114703880+Okawuma@users.noreply.github.com>
2023-01-06 05:36:11 +0000

    Update wallfile.sh

--- wallfile.sh
+++ wallfile.sh
@@ -2,3 +2,9 @@ Wall are made of stone, sand and cement.
 They are impenetrable.
 Though i have read and heard that some people can pass through walls
 These normally have the spirit of the living God resident in them.
+They are those who are born again or saved.
+They can do anything they believe their God for.
+For them, everything is possible.
+Its amazing to live with and commune with them.
+Positivity comes easy for them.
+And actually,thats what moves everything down here, POSITIVE ENERGY.
