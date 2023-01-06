0c167474a5b96adab739660848ac72b2b03326c2
Otillia <kawumatk@gmail.com>
2023-01-06 05:30:38 +0000

    holystaff

--- wallfile.sh
+++ wallfile.sh
@@ -1,2 +1,4 @@
 Wall are made of stone, sand and cement.
 They are impenetrable.
+Though i have read and heard that some people can pass through walls
+These normally have the spirit of the living God resident in them.
