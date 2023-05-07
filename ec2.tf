69554f59e48c216ab2606b18f7221bee9216a9d1
Otillia <114703880+Okawuma@users.noreply.github.com>
2023-05-07 02:54:35 +0100

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,35 +1,31 @@
-# Terraform Settings Block
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0" # Optional but recommended in production
+      version = "~> 3.0"
     }
   }
 }
 
-# Provider Block
 provider "aws" {
-  #profile = "default" #credentials
+
   region  = "eu-west-2"
 }
 
 terraform {
   backend "s3" {
     bucket         = "my-backend-2"
-    key            = "my-backend-2/terraform/terraform.tfstate" #remote location
+    key            = "my-backend-2/terraform/terraform.tfstate" 
     region         = "eu-west-2"
     dynamodb_table = "my-bucket-2-table"
   }
-# Resource Block
 
 resource "aws_instance" "ec2demo" {
-  ami           = "ami-09744628bed84e434" # Amazon Linux in eu-west-2, update as per your region
+  ami           = "ami-09744628bed84e434"
   instance_type = "t2.micro"
 
   tags = {
     Name = "OTILLIA-SERVER2"
   }
  }
-}
 }
