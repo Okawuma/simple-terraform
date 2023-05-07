3f88eddbc053d360b3d0c5b7ad6aa000fb9d4cb5
Otillia <114703880+Okawuma@users.noreply.github.com>
2023-05-07 03:46:26 +0100

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -14,13 +14,13 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = "my-backend-2"
-    key            = "my-backend-2/terraform/terraform.tfstate" 
+    bucket         = "my-backend-bucket1"
+    key            = "my-backend-bucket1/terraform/terraform.tfstate" 
     region         = "eu-west-2"
-    dynamodb_table = "my-bucket-2-table"
+    dynamodb_table = "my-bucketable-2"
   }
 }
-resource "aws_instance" "ec2demot" {
+resource "aws_instance" "Archie" {
   ami           = "ami-0cbf73a24cc7ff66b"
   instance_type = "t2.micro"
 
